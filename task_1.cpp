fa12bbe905d7e694131321fd4effbee7980103c4
NattSss11 <129979232+NattSss11@users.noreply.github.com>
2023-10-20 08:13:02 +0300

    Update task_1.cpp

--- task_1.cpp
+++ task_1.cpp
@@ -8,26 +8,27 @@ using namespace std;
 class EBook {
 public:
     EBook()
-        : _users(MAX_USERS_COUNT + 1, -1)
-        , _pages(MAX_PAGES_COUNT, 0)
+        : _users_to_page(MAX_USERS_COUNT + 1, -1)
+        , _page_to_users(MAX_PAGES_COUNT, 0)
     {}
 
     void Read(int user, int page) {
-        int current_page = _users[user] + 1;
+        int current_page = _users_to_page[user] + 1;
         while (current_page <= page) {
-            ++_pages[current_page];
+            ++_page_to_users[current_page];
             ++current_page;
         }
-        _users[user] = page;
+        _users_to_page[user] = page;
     }
 
     double Cheer(int user) {
-        if (_users[user] == -1)
+        if (_users_to_page[user] == -1)
             return 0;
-        if (_pages[0] == 1)
+        int count = _page_to_users[0];
+        if (count == 1)
             return 1;
-        int pages_read = _pages[0] - _pages[_users[user]];
-        return static_cast<double>(pages_read) / (_pages[0] - 1);
+        int pages_read = count - _page_to_users[_users_to_page[user]];
+        return static_cast<double>(pages_read) / (count - 1);
     }
 
     void ReadEBook(istream& input, ostream& output ) {
@@ -48,8 +49,8 @@ public:
     }
 
 private:
-    vector<int> _users;
-    vector<int> _pages;
+    vector<int> _users_to_page;
+    vector<int> _page_to_users;
 
     static const int MAX_USERS_COUNT = 100000;
     static const int MAX_PAGES_COUNT = 1000;
