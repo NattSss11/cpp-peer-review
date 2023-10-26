08236cb50935475921605ea3028dea46650a4994
NattSss11 <129979232+NattSss11@users.noreply.github.com>
2023-10-26 16:42:28 +0300

    Update CheckDateTimeValidity.cpp

--- CheckDateTimeValidity.cpp
+++ CheckDateTimeValidity.cpp
@@ -5,13 +5,24 @@ void CheckRange(int value, int min_value, int max_value, const string& error_mes
 }
 
 void CheckDateTimeValidity(const DateTime& dt) {
-    CheckRange(dt.year, 1, 9999, "year is out of range"s);
-    CheckRange(dt.month, 1, 12, "month is out of range"s);
-
+    CheckRange(dt.year, MIN_YEAR, MAX_YEAR, "year is out of range"s);
+    CheckRange(dt.month, MIN_MONTH, MAX_MONTH, "month is out of range"s);
     const bool is_leap_year = (dt.year % 4 == 0) && !(dt.year % 100 == 0 && dt.year % 400 != 0);
     const array<int, 12> month_lengths = {31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    CheckRange(dt.day, 1, month_lengths[dt.month - 1], "day is out of range"s);
-    CheckRange(dt.hour, 0, 23, "hour is out of range"s);
-    CheckRange(dt.minute, 0, 59, "minute is out of range"s);
-    CheckRange(dt.second, 0, 59, "second is out of range"s);
+    CheckRange(dt.day, MIN_DAY, month_lengths[dt.month - 1], "day is out of range"s);
+    CheckRange(dt.hour, MIN_HOUR, MAX_HOUR, "hour is out of range"s);
+    CheckRange(dt.minute, MIN_MIN, MAX_MIN, "minute is out of range"s);
+    CheckRange(dt.second, MIN_SEC, MAX_SEC, "second is out of range"s);
 }
+
+const static int MAX_YEAR = 9999;
+const static int MIN_YEAR = 1;
+const static int MAX_MONTH = 12;
+const static int MIN_MONTH = 1;
+const static int MIN_DAY = 1;
+const static int MAX_HOUR = 23;
+const static int MIN_HOUR = 0;
+const static int MAX_MIN = 59;
+const static int MIN_MIN = 0;
+const static int MAX_SEC = 59;
+const static int MIN_SEC = 0;
